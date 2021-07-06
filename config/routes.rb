ff8ea6b010fbb69ccc97fb9ca44eaa6571bf31a4
petitejess <jessica.gozali@outlook.com.au>
2021-07-06 16:59:03 +1000

    Add footer and dynamic year copyright

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :user_stocks
   resources :categories
   devise_for :users
+
+  get '/user', to: 'user_stocks#index' , as: 'user_root'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
